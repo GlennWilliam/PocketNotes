52559734887729c83a5f34b36607e106fd6fc2f5
GlennWilliam <glennwilliamm@gmail.com>
2025-10-08 01:13:31 -0700

    Update user and profile/thumbnail picture

--- Backend/app/routes/user_route.py
+++ Backend/app/routes/user_route.py
@@ -1,9 +1,10 @@
-from app.services.user_service import get_user_by_id
+from app.services.user_service import get_user_by_id, update_user_by_id
 from flask import Blueprint, jsonify, request
 from flask_jwt_extended import jwt_required, get_jwt_identity
 from app.utils.response import success_response, error_response
 
 user_bp = Blueprint('user_bp', __name__)
+
 
 @user_bp.route('/', methods=['GET'])
 @jwt_required()
@@ -14,5 +15,24 @@ def get_user():
 	if not user:
 		return error_response(message, 404)
 
-	return success_response(user, message, 200)	
+	return success_response(user, message, 200)
+
+
+@user_bp.route('/', methods=['PUT'])
+@jwt_required()
+def update_user():
+	user_id = get_jwt_identity()
+	data = request.form.to_dict()
+	profile_picture = request.files.get('profile_picture')
+	thumbnail_picture = request.files.get('thumbnail_picture')
+
+	if not data and not profile_picture and not thumbnail_picture:
+		return error_response("No data provided", 400)
+
+	user, message = update_user_by_id(user_id, data, profile_picture, thumbnail_picture)
+
+	if not user:
+		return error_response(message, 400)
+	
+	return success_response(user, message, 200)
 
--- Backend/app/services/user_service.py
+++ Backend/app/services/user_service.py
@@ -1,3 +1,5 @@
+import os, uuid
+from werkzeug.utils import secure_filename
 from app.models.user import User
 from app import db
 
@@ -8,3 +10,69 @@ def get_user_by_id(user_id):
 		return None, "User not found"
 	
 	return user.to_json(), "User retrieved successfully"
+
+def update_user_by_id(user_id, data, profile_picture=None, thumbnail_picture=None):
+	user = User.query.get(user_id)
+	
+	if not user:
+		return None, "User not found"
+	
+	try:
+		if 'password' in data and data['password']:
+			user.set_password(data['password'])
+
+		for field in ['username', 'email']:
+			if field in data and data[field]:
+				setattr(user, field, data[field])
+		
+		
+		os.makedirs('uploads/', exist_ok=True)
+		
+		# Handle profile picture upload
+		if profile_picture and is_valid_image(profile_picture.filename):
+			name = random_name(profile_picture.filename)
+			filename = secure_filename(name)
+			path = os.path.join('uploads', filename)
+			profile_picture.save(path)
+			
+			if user.profile_picture:
+				filename_old = user.profile_picture.replace('/uploads/', '')
+				path_old = os.path.join('uploads', filename_old)
+				if os.path.exists(path_old):
+					os.remove(path_old)
+			
+			setattr(user, 'profile_picture', f'/uploads/{filename}')
+			
+
+		# Handle thumbnail picture upload
+		if thumbnail_picture and is_valid_image(thumbnail_picture.filename):
+			name = random_name(thumbnail_picture.filename)
+			filename = secure_filename(name)
+			path = os.path.join('uploads', filename)
+			thumbnail_picture.save(path)
+
+			if user.thumbnail_picture:
+				filename_old = user.thumbnail_picture.replace('/uploads/', '')
+				path_old = os.path.join('uploads', filename_old)
+				if os.path.exists(path_old):
+					os.remove(path_old)
+
+			setattr(user, 'thumbnail_picture', f'/uploads/{filename}')
+
+		db.session.commit()
+		return user.to_json(), f"User {user.username} updated successfully"
+
+	except Exception as e:
+		db.session.rollback()
+		return None, "Error updating user: " + str(e)
+	
+def is_valid_image(filename):
+	allowed_extensions = {'png', 'jpg', 'jpeg', 'gif'}
+	_, ext = os.path.splitext(filename.lower())
+	return ext in allowed_extensions
+
+def random_name(filename):
+	# get extension of the file
+	ext = os.path.splitext(filename)[1].lower()
+	# generate a random name
+	return f"{uuid.uuid4().hex}.{ext}"
