428d9132d7d298df2739f38a954db450bd935693
GlennWilliam <glennwilliamm@gmail.com>
2025-11-06 00:21:51 -0800

    CORS and refine auth endpoint

--- Backend/app/__init__.py
+++ Backend/app/__init__.py
@@ -4,6 +4,7 @@ from flask_bcrypt import Bcrypt
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_jwt_extended import JWTManager
+from flask_cors import CORS
 
 bcrypt = Bcrypt()
 db = SQLAlchemy()
@@ -25,25 +26,56 @@ def create_app():
 	# Register table
 	from app.models.user import User
 	from app.models.notes import Notes
+
+	CORS(app, resources={
+		r"/api/*": {
+			"origins": [
+				"http://localhost:3000",
+				"http://127.0.0.1:3000"
+			],
+			"supports_credentials": True,
+			"allow_headers": [
+				"Content-Type",
+				"Authorization"
+			],
+			"methods": ["GET", "POST", "PUT", "DELETE", "OPTIONS"]
+		}
+	})
+
 	
 	# Routing
+	
+	# --- TESTING PURPOSE ---
 	from app.routes.base_route import base
 	app.register_blueprint(base, url_prefix='/')
 	
-	from app.routes.auth_route import register_bp
-	app.register_blueprint(register_bp, url_prefix='/api/v1/register')
+	# [POST] /api/v1/auth/login -> Login
+	# [POST] /api/v1/auth/register -> Register
+	from app.routes.auth_route import auth_bp
+	app.register_blueprint(auth_bp, url_prefix='/api/v1/auth')
 	
-	from app.routes.auth_route import login_bp
-	app.register_blueprint(login_bp, url_prefix='/api/v1/login')
-	
+	# [GET] /api/v1/user/<string:user_id> -> Get user by ID
+	# [PUT] /api/v1/user/<string:user_id> -> Update user by
 	from app.routes.user_route import user_bp
 	app.register_blueprint(user_bp, url_prefix='/api/v1/user')
-	
+
+	# [GET] /api/v1/uploads/<filename> -> Get uploaded file
 	from app.routes.upload_route import upload_bp
-	app.register_blueprint(upload_bp, url_prefix='/api/v1/upload')
+	app.register_blueprint(upload_bp, url_prefix='/api/v1/uploads')
 	
+	# [GET] /api/v1/note/ -> Get all notes
+	# [POST] /api/v1/note/ -> Create a note
+	# [GET] /api/v1/note/me -> Get user's notes
+	# [GET] /api/v1/note/<string:slug> -> Get note by slug
+	# [PUT] /api/v1/note/<string:note_id> -> Update note by ID
+	# [DELETE] /api/v1/note/<string:note_id> -> Delete note by ID
 	from app.routes.note_route import note_bp
 	app.register_blueprint(note_bp, url_prefix='/api/v1/note')
+	
+	# [POST] /api/v1/like/<string:note_id> -> Toggle like on a note
+	from app.routes.like_route import like_bp
+	app.register_blueprint(like_bp, url_prefix='/api/v1/like')
+
 	
 
 	return app
